ec03b51609ec4ec06460b70ec1ddff08bb580f17
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-18 20:36:00 -0300

    feat: add emojis

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,21 @@
-import { Container } from './styles'
+import { useMemo } from 'react'
+
+import emojis from 'utils/emojis'
+import { Container, Profile, Welcome, UserName } from './styles'
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length)
+    return emojis[index]
+  }, [])
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Felipe Santos</UserName>
+      </Profile>
     </Container>
   )
 }
