02246601661145a50680fa41bfac8c8e4a84db56
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-18 17:08:26 -0300

    feat: add styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/globalStyles'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>My Wallet</title>
+        <link rel="shortcut icon" href="/wallet.svg" />
+        <link rel="apple-touch-icon" href="/wallet.svg" />
+        <meta name="description" content="A financial dashboard project" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
